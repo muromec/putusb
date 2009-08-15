2fbb316e44dcc2af82a8268c5195ce295fb12c8e
Ilya Petrov <ilya.muromec@gmail.com>
2009-08-15 04:08:43 +0300

    add threading. zomg

--- qputusb.py
+++ qputusb.py
@@ -5,6 +5,18 @@
 import sys
 from PyQt4 import QtGui, QtCore
 import putusb
+from threading import Thread
+
+from time import sleep
+
+def bg(func):
+  def func_bg(*args):
+
+    t = Thread(target=func, args=args)
+    t.setDaemon(True)
+    t.start()
+
+  return func_bg
 
 
 class Main(QtGui.QWidget):
@@ -21,6 +33,8 @@ class Main(QtGui.QWidget):
         fbtn = QtGui.QPushButton("Flash")
         clr = QtGui.QPushButton("Clear")
         txt = QtGui.QTextEdit()
+        namelbl = QtGui.QLabel()
+        statelbl = QtGui.QLabel()
 
         cmd = QtGui.QLineEdit()
         id_select = QtGui.QComboBox()
@@ -38,12 +52,19 @@ class Main(QtGui.QWidget):
         self.connect( wcfg, click, self.setInfo )
         self.connect( fbtn, click, self.flash )
         self.connect( clr, click, txt.clear )
+        self.connect(
+            self, QtCore.SIGNAL('select'),
+            self.selectFiles
+        )
+
 
         vbox = QtGui.QVBoxLayout()
 
         # first line
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(find)
+        hbox.addWidget(namelbl)
+        hbox.addWidget(statelbl)
         hbox.addStretch(1)
         hbox.addWidget(rcfg)
         hbox.addWidget(wcfg)
@@ -70,10 +91,18 @@ class Main(QtGui.QWidget):
         self.log = txt
         self.cmdline = cmd
         self.machid = id_select
+        self.name = namelbl
+        self.statelbl = statelbl
+
+        self.flash = None
 
 
     def inf(self, text):
         self.log.append(text)
+        print text
+
+    def state(self, state):
+        self.statelbl.setText(state)
 
     def config(self):
         idx = self.machid.currentIndex()
@@ -85,7 +114,7 @@ class Main(QtGui.QWidget):
 
         return putusb.encode_params(cmdline, machid)
 
-
+    @bg
     def showInfo(self):
 
         self.inf("version: %s"%self.dev.version())
@@ -93,17 +122,18 @@ class Main(QtGui.QWidget):
 
         if 'read' not in dir(self.dev):
           self.inf("memory read not supported")
+          self.inf("loading blob to read memory")
           self.sendBlob()
 
         magic = self.dev.get(0x000c0000+131072-4,4)
         magic = putusb.decode_bytes(magic)
 
-
-
         if magic == 0xdeadb007:
+          self.inf("magic found")
 
+          self.state("reading config")
           cfg = self.dev.get(0x000c0000,131072)
-          self.inf("genblob configuration")
+          self.inf("genblob config:")
 
           line, machid, magic = putusb.decode_params(cfg)
           self.cmdline.setText(line)
@@ -116,12 +146,17 @@ class Main(QtGui.QWidget):
             print idx
 
           self.inf("cmdline: %s"%line)
-          self.inf("machine: %d"%machid)
+          self.inf("machine: %s (%d)"%(putusb.machids[machid], machid))
           self.inf("magic: %x"%magic)
         else:
+          self.inf("magic not found")
           self.inf("genblob not configured")
 
+        self.state("")
+
+    @bg
     def setInfo(self):
+        self.state("writing config")
         cfg = self.config()
 
         if len(cfg) != 131072:
@@ -136,37 +171,80 @@ class Main(QtGui.QWidget):
         self.dev.flash(0x000c0000, cfg)
 
         self.inf("config set")
+        self.state("")
 
+    @bg
     def findDev(self):
-        self.log.clear()
-
+        self.state("looking for device")
         try:
           self.dev = putusb.MotoUsb()
-          self.inf("device found: %s"%self.dev.name())
+          name = self.dev.name()
         except:
-          self.inf("no device")
+          name = "no device"
+
+        self.name.setText(name)
+        self.state("")
 
     def sendBlob(self):
+        self.dev.version()
+        self.dev.serial()
+
+        self.state("loading blob")
         self.dev.set(0xa1000000, putusb.encode_bytes(0x0D3ADCA7))
         self.dev.run_file("blob")
 
         while self.dev.dev.idProduct != 0xbeef:
           self.findDev()
+          sleep(0.3)
 
-    def flash(self):
+        self.state("")
 
-        self.showInfo()
-
-        self.inf("flash")
-
+    def selectFiles(self):
+        print "select files"
         fdialog = QtGui.QFileDialog()
         fdialog.exec_()
 
-        path = str(fdialog.selectedFiles()[0])
+        self.flash = str(fdialog.selectedFiles()[0])
+
+        print "selected"
+
+
+    @bg
+    def flash(self):
+
+        if 'flash' not in dir(self.dev):
+          self.inf("memory read not supported")
+          self.inf("loading blob to read memory")
+          self.sendBlob()
+
+        self.inf("flash")
+
+        # viva bydlocode!
+        # dont even want to know, how this should be
+        # done with suckfull signal-signal-by-slot oops.
+        # damn qt and damn python segfaults when 
+        # trying to run fdialog from here
+        self.emit(QtCore.SIGNAL('select'))
+
+        while self.flash == None:
+          sleep(0.1)
+
+        if self.flash == False:
+            self.flash = None
+            return
+
+        path = self.flash
 
         self.inf("going to flash %s"%path)
 
-        self.dev.flash_index(path)
+        self.state("flashing")
+        try:
+          self.dev.flash_index(path)
+          self.inf("ok")
+        except:
+          self.inf("fail")
+
+        self.state("")
 
 app = QtGui.QApplication(sys.argv)
 w = Main()
