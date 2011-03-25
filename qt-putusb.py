abe896f3c85031d06c2f622dde2a798caf5085ae
Ulric Wilfred <shaman.sir@gmail.com>
2011-03-26 00:14:39 +0200

    widgets change draft

--- qt-putusb.py
+++ qt-putusb.py
@@ -1,45 +1,91 @@
 import sys
 import os
+import time
 from PyQt4 import QtGui, QtCore
+from threading import Thread
 
-def test_connection():
-    time.sleep(10)
+def bg(func):
+  def func_bg(*args):
+
+    t = Thread(target=func, args=args)
+    t.setDaemon(True)
+    t.start()
+
+  return func_bg
+
+@bg
+def wait_connection():
+    time.sleep(4)
+    print "connected"
+    return True
 
 class ConnectionWidget(QtGui.QWidget):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        hbox = QtGui.QHBoxLayout()
+        vbox = QtGui.QVBoxLayout()
 
-        push1 = QtGui.QPushButton("Push Me 1")
-        push2 = QtGui.QPushButton("Push Me 2")
+        self.status = QtGui.QLabel("Not connected")
+        # self.icon = QtGui.QImage(128, 128, QtGui.QImage.Format_RGB32)
+        self.icon = QtGui.QDial()
+        self.advice = QtGui.QLabel("Connect device...")
 
-        hbox.addWidget(push1)
-        hbox.addWidget(push2)
+        vbox.addWidget(self.status)
+        vbox.addWidget(self.icon)
+        vbox.addWidget(self.advice)
 
-        self.setLayout(hbox)
+        self.setLayout(vbox)
+
+    def show_connected(self):
+        print "showing connected"
+        self.status.setText("Connected")
+        # update icon
+        self.advice.setText("Device found")
 
 class FlashingWidget(QtGui.QWidget):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-class QPWindow(QtGui.QMainWindow):
+        hbox = QtGui.QHBoxLayout()
+
+        push1 = QtGui.QPushButton("Push 1")
+        push2 = QtGui.QPushButton("Push 2")
+
+        hbox.addWidget(push1)
+        hbox.addWidget(push2)
+
+        self.setLayout(hbox)
+
+class PUWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
 
         self.setWindowTitle('PutUSB')
 
+class PUApplication(QtGui.QApplication):
+
+    def __init__(self, args):
+        QtGui.QApplication.__init__(self, args)
+
+        self.win = PUWindow()
+
         self.conn_widget = ConnectionWidget()
         self.flsh_widget = FlashingWidget()
 
-        self.setCentralWidget(self.conn_widget)
+        self.win.setCentralWidget(self.conn_widget)
 
+    def run_(self):
+        self.win.show()
 
-app = QtGui.QApplication(sys.argv)
-win = QPWindow()
-win.show()
+        if wait_connection():
+            self.conn_widget.show_connected()
+            time.sleep(2)
+            self.win.setCentralWidget(self.flsh_widget)
+
+app = PUApplication(sys.argv)
+app.run_()
 sys.exit(app.exec_())
 
