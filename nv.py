0146cd697d2c85cc682985da96bd20205126cb70
Ilya Petrov <ilya.muromec@gmail.com>
2011-08-23 04:40:05 +0300

    some crap about sbk

--- nv.py
+++ nv.py
@@ -3,6 +3,7 @@ import putusb
 dev = putusb.NvidiaUsb()
 #dev.boot("bin/tegra_pre_boot.bin", "bin/fastboot.stock.bin")
 print putusb.Usb.recv(dev)
+
 f = open('dump_0.bin')
 dev.send(f.read())
 f.close()
@@ -11,12 +12,27 @@ print dev.recv()
 
 dev.send_pre('bin/dump_1.bin')
 
+print dev.recv()
+
 dev.cmd(1,1,4,4,0x0ff0)
 
-f = open('bin/dump_0.bin')
-dev.send(f.read())
-f.close()
-dev.send('\x6e\xa1\xff\xff')
+print dev.recv()
+
+state = putusb.LoadState('bin/dump_2.bin')
+while state.size:
+      map(dev.send, state.feed())
+      dev.recv_ack()
+
+print dev.recv()
+
+dev.send_ack()
+
+# send odm data
+dev.cmd(1,3,4,6,0x300d8011)
+dev.recv_ack()
+
+print dev.recv()
+
 dev.send_ack()
 
 dev.send_loader('bin/dump_3.bin')
